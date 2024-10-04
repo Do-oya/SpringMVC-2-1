ea12061d0362ba900bc1a5bb35466e8207265b96
Do-oya <qkrwjdgus1441@naver.com>
2024-10-05 00:01:26 +0900

    [김영한 스프링 MVC 2편 - 백엔드 웹 개발 활용 기술]

--- src/main/java/hello/thymeleafbasic/ThymeleafBasicApplication.java
+++ src/main/java/hello/thymeleafbasic/ThymeleafBasicApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ThymeleafBasicApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ThymeleafBasicApplication.class, args);
     }
-
 }
